dd670820692be109aa88183906f9e50c6f47306a
sontixyou <kengoviolinphoto@gmail.com>
2025-01-05 18:17:15 +0900

    add todos path

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Welcome to the Express + TypeScript Server!" });
-});
+})
+  .get("/todos", (req: Request, res: Response) => {
+    res.json({ message: "List of all todos" });
+  });
 
 app.listen(port, () => {
   console.log(`The server is running at http://localhost:${port}`);
