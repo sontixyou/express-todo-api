a8fbf331f5e6569d31e0b1a9e95f7ab201af6f49
sontixyou <kengoviolinphoto@gmail.com>
2025-01-05 18:19:09 +0900

    change response

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,25 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Welcome to the Express + TypeScript Server!" });
 })
   .get("/todos", (req: Request, res: Response) => {
-    res.json({ message: "List of all todos" });
+    res.json({
+      "todos": [
+        {
+          "id": 1,
+          "title": "Do laundry",
+          "completed": false
+        },
+        {
+          "id": 2,
+          "title": "Clean room",
+          "completed": true
+        },
+        {
+          "id": 3,
+          "title": "Study for exam",
+          "completed": false
+        }
+      ]
+    });
   });
 
 app.listen(port, () => {
